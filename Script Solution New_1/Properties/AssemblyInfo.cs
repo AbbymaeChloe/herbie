78ed95bba18ed8d0c0e72ce8391ef0189c659290
herbert.santiago <herbert.santiago@SGW1022NB0340>
2023-06-05 15:01:51 +0800

    Add project files.

---
+++ Script Solution New_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Script Solution New_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("Script Solution New_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("49F2446E-56A4-4718-8E57-735F9FE43294")]
+[assembly: AssemblyVersion("1.0.0.0")]
